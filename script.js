0cccb59bfc135a83f329979515cfce388712aa88
tun-source <antoine.hurtaud@icloud.com>
2024-12-28 18:51:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,11 @@
 document.querySelector('.burger').addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('active');
   });
-  
+  document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+});
