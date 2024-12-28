d45506899e10b32bb33ece9affb79bcbdb750dbe
tun-source <antoine.hurtaud@icloud.com>
2024-12-28 18:58:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Toggle navbar menu
-document.querySelector('.burger').addEventListener('click', () => {
-    document.querySelector('.menu').classList.toggle('active');
-  });
+
   document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.burger');
   const menu = document.querySelector('.menu');
